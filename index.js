474abfb7d4fa6c4d81c38a7173794333aaa64ddc
Carverok <nayib.ayus@gmail.com>
2018-04-08 21:22:55 -0500

    DontEnv-Feature

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 const app = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.LocalPort || 3000;
 
 app.listen(port, () => console.log(`server running at localhost:${port}`));
